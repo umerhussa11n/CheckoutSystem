deabfaae3da00ad5e3b14b2adf87990864da0f8c
umer hussain <umerhussa11n@gmail.com>
2018-05-03 10:57:01 +0100

    Initial Commit for Checkout System

---
+++ SportsShop/Controllers/AboutUsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsShop.Controllers
+{
+    public class AboutUsController
+    {
+
+        public string Address()
+        {
+            return "2A Clifford Road";
+        }
+
+        public string Phone()
+        {
+            return "07737726770";
+        }
+    }
+}
---
+++ SportsShop/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SportsShop
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app,
+                              IHostingEnvironment env,
+                              IConfiguration configuration,
+                              IGreeter greeter,
+                              ILogger<Startup> logger
+                              )
+        {
+
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+            }
+
+            // Displays files in the wwwroot folder and run the index..
+            //app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            // this has the MVC Default Routs 
+            //app.UseMvcWithDefaultRoute();
+
+            // Creating a route overself..
+            app.UseMvc(configureRouts);
+
+            app.Run(async (context) =>
+            {
+                //var greeting = configuration["Greeting"];
+                var greeting = greeter.getMessageOftheDay();
+                await context.Response.WriteAsync($"{greeting} : {env.EnvironmentName }");
+            });
+        }
+
+        private void configureRouts(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute("Default",
+                "{Controller=Home}/{action=Index}/{Id?}");
+        }
+    }
+}
